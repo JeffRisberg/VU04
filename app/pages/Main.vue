d7dbeacc0d32fc7049bf246727792632cbfe2791
JeffRisberg <jrisberg@aol.com>
2017-12-08 19:46:50 -0800

    populated incidents

--- app/pages/Main.vue
+++ app/pages/Main.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
+  <div class="main">
+    main page
   </div>
 </template>
 
@@ -9,7 +9,6 @@ export default {
   name: 'Main',
   data () {
     return {
-      msg: 'VU04'
     }
   }
 }
