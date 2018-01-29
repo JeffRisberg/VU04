ce8e776983a642d0f4c320bd0a640a41da2c268d
JeffRisberg <jrisberg@aol.com>
2018-01-28 19:11:26 -0800

    implemented two modes of display

---
+++ app/aiseraComponents/MajorIncidentCard.vue
@@ -0,0 +1,24 @@
+<template>
+  <b-card>
+    <div class="h5 text-left">
+      <i class="fa fa-cogs text-muted"></i> Services ({{count || 0}})
+    </div>
+    <slot v-for="row in item_list || []">
+      <div class="score-small">
+        <div :class="'score-small-icon score-bg-' + row.score"
+        >{{row.score}}</div>
+      </div>
+      <div class="mb-3" style="overflow:hidden;
+        text-overflow:ellipsis;white-space:nowrap;"
+           :data-id="row.id"
+      >{{row.name}}</div>
+    </slot>
+  </b-card>
+</template>
+
+<script>
+  export default {
+    name: 'MajorServicesCard',
+    props: ['count', 'item_list']
+  }
+</script>
--- app/pages/MajorIncident.vue
+++ app/pages/MajorIncident.vue
@@ -12,17 +12,65 @@
         <indicator title="Cost Savings" value="$345,678"/>
       </div>
     </div>
+
+    <b-button class="btn btn-primary" v-if="mode !== 'details'" @click="changeMode('details')">
+      <i class="fa fa-angle-double-right" aria-hidden="true"></i>
+      Details
+    </b-button>
+
+    <b-button class="btn btn-primary" v-if="mode !== 'summary'" @click="changeMode('summary')">
+      <i class="fa fa-angle-double-right" aria-hidden="true"></i>
+      Summary
+    </b-button>
+
+    <major-incident-card :count="count" :item_list="item_list"/>
+
+    <b-row class="mb-4" style="background: #eee;" v-if="mode === 'details'">
+      <b-col sm="12">
+        <data-table :fields="tableFields"
+                    :paginated="false"
+                    :dataSet="relatedIncidents"/>
+      </b-col>
+    </b-row>
   </div>
 </template>
 
 <script>
+  import DataTable from '../aiseraComponents/DataTable.vue'
   import Indicator from '../aiseraComponents/Indicator.vue'
+  import MajorIncidentCard from '../aiseraComponents/MajorIncidentCard.vue'
 
   export default {
     name: 'MajorIncident',
-    components: { Indicator },
+    components: { DataTable, Indicator, MajorIncidentCard },
     data () {
-      return {}
+      return {
+        mode: 'summary',
+        count: 5,
+        item_list: [
+          { name: 'a', score: 5 },
+          { name: 'b', score: 6 }
+        ],
+        tableFields: [
+          { key: 'name', sortable: true },
+          { key: 'description', sortable: true }
+        ],
+        relatedIncidents: [
+          {
+            name: 'INC_001',
+            description: 'desc1'
+          },
+          {
+            name: 'INC_002',
+            description: 'desc2'
+          }
+        ]
+      }
+    },
+    methods: {
+      changeMode: function (newMode) {
+        this.mode = newMode
+      }
     }
   }
 </script>
